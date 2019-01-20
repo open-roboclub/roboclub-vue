dd88457c75045d15c9a12d0f6d72c940bad0e862
Haider Ali <haider.lee23@gmail.com>
2019-01-20 19:12:18 +0530

    WIP: home page

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
 Vue.use(Vuetify, {
   iconfont: 'mdi',
