358461c0c192639563d81cdad938da14314bb10c
Haider Ali <haider.lee23@gmail.com>
2019-01-22 00:06:33 +0530

    revert mdi icon installation

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
 Vue.use(Vuetify, {
   iconfont: 'mdi',
