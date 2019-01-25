f25775396b7edf5d0468d00755c0da19bc2734b1
Haider Ali <haider.lee23@gmail.com>
2019-01-25 20:32:49 +0530

    fix lint errors

--- src/components/Other/Projects.vue
+++ src/components/Other/Projects.vue
@@ -4,19 +4,17 @@
       <v-flex xs10 sm6 offset-sm3 offset-xs1>
         <v-card class="mt-0 mb-4" color="#BF3EFF">
           <v-card-text>
-            <div>
-              <h1 class="projects-header">Completed Projects</h1>
-            </div>
+            <div><h1 class="projects-header">Completed Projects</h1></div>
           </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
     <v-layout row wrap>
       <v-hover
-      v-for="project in projects"
-      :key="project"
-      class="mb-3"
-      v-if="!project.ongoing"
+        v-for="project in projects"
+        :key="project"
+        class="mb-3"
+        v-if="!project.ongoing"
       >
         <v-card
           slot-scope="{ hover }"
@@ -25,28 +23,24 @@
           style="cursor: pointer"
           class="mx-auto"
           width="450"
-        ><br>
-        <v-flex xs12 class="text-xs-center">
-          <v-avatar
-            :tile="false"
-            :size="250"
-            color="grey lighten-4"
-          >
-            <v-img
-              :aspect-ratio="16/9"
-              :src="project.image"
-              alt="Avatar"
-            ></v-img>
-          </v-avatar>
-        </v-flex>
+          ><br />
+          <v-flex xs12 class="text-xs-center">
+            <v-avatar :tile="false" :size="250" color="grey lighten-4">
+              <v-img
+                :aspect-ratio="16 / 9"
+                :src="project.image"
+                alt="Avatar"
+              ></v-img>
+            </v-avatar>
+          </v-flex>
           <v-card-title>
             <v-flex xs12 class="text-xs-center">
-                <span class="headline">{{ project.name }}</span>
-                <div class="d-flex">
-                  <div class="ml-2 grey--text text--darken-2">
-                    <span>{{ project.team }}</span>
-                  </div>
+              <span class="headline">{{ project.name }}</span>
+              <div class="d-flex">
+                <div class="ml-2 grey--text text--darken-2">
+                  <span>{{ project.team }}</span>
                 </div>
+              </div>
             </v-flex>
           </v-card-title>
         </v-card>
@@ -59,20 +53,13 @@
           transition="dialog-bottom-transition"
         >
           <v-card>
-            <v-card-title
-              class="headline grey lighten-2"
-              primary-title
-            >
+            <v-card-title class="headline grey lighten-2" primary-title>
               {{ selectedProject.name }}
             </v-card-title>
             <v-flex xs12 class="text-xs-center mt-2 mb-0">
-              <v-avatar
-                :tile="false"
-                :size="225"
-                color="grey lighten-4"
-              >
+              <v-avatar :tile="false" :size="225" color="grey lighten-4">
                 <v-img
-                  :aspect-ratio="16/9"
+                  :aspect-ratio="16 / 9"
                   :src="selectedProject.image"
                   alt="Avatar"
                 ></v-img>
@@ -84,18 +71,10 @@
             <v-divider></v-divider>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn
-                color="green darken-1"
-                flat
-                @click="openProject()"
-              >
+              <v-btn color="green darken-1" flat @click="openProject()">
                 Know More
               </v-btn>
-              <v-btn
-                color="primary"
-                flat
-                @click="dialog = false"
-              >
+              <v-btn color="primary" flat @click="dialog = false">
                 Close
               </v-btn>
             </v-card-actions>
