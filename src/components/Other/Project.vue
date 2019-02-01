9cff9fe6490f8e1ebda28eef11c5ee83fb991674
Haider Ali <haider.lee23@gmail.com>
2019-02-02 00:20:50 +0530

    Connect firebase only when project store null

--- src/components/Other/Project.vue
+++ src/components/Other/Project.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-layout row wrap>
-      <v-flex xs12 xl8 offset-xl2>
+      <v-flex xs12 xl8 offset-xl2 v-if="loadedProject == null">
         <v-card>
           <v-card-title>
             <div>
@@ -38,12 +38,49 @@
           </v-card-text>
         </v-card>
       </v-flex>
+      <v-flex xs12 xl8 offset-xl2 v-else>
+        <v-card>
+          <v-card-title>
+            <div>
+              <h2 class="primary--text">{{ loadedProject.name }}</h2>
+              <div class="primary--text">{{ loadedProject.team }}</div>
+            </div>
+          </v-card-title>
+          <v-flex xs12 class="text-xs-center mt-2 mb-0">
+            <v-avatar
+              v-if="!loadedProject.images"
+              :tile="false"
+              :size="225"
+              color="grey lighten-4"
+            >
+              <v-img
+                :aspect-ratio="16 / 9"
+                :src="loadedProject.image"
+                alt="Avatar"
+              ></v-img>
+            </v-avatar>
+            <v-carousel v-if="loadedProject.images">
+              <v-carousel-item
+                v-for="image in loadedProject.images"
+                :src="image"
+                :key="image"
+              >
+              </v-carousel-item>
+            </v-carousel>
+          </v-flex>
+          <v-card-text>
+            <div>
+              <p style="font-size: 18px;">{{ loadedProject.description }}</p>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions, mapGetters } from 'vuex'
 
 export default {
   metaInfo: {
@@ -56,28 +93,34 @@ export default {
     }
   },
   computed: {
+    loadedProject() {
+      return this.getProjectById()(this.id)
+    },
     ...mapState('projects', ['project'])
   },
   methods: {
-    ...mapActions('projects', ['setProjectRef'])
+    ...mapActions('projects', ['setProjectRef']),
+    ...mapGetters('projects', ['getProjectById'])
   },
   created() {
-    this.setProjectRef({
-      ref: this.$firebase
-        .database()
-        .ref('projects')
-        .orderByChild('id')
-        .equalTo(this.id),
-      callbacks: {
-        readyCallback: () => {
-          this.loading = false
-        },
-        cancelCallback: error => {
-          console.error(error)
-          this.loading = false
+    if (this.loadedProject == null) {
+      this.setProjectRef({
+        ref: this.$firebase
+          .database()
+          .ref('projects')
+          .orderByChild('id')
+          .equalTo(this.id),
+        callbacks: {
+          readyCallback: () => {
+            this.loading = false
+          },
+          cancelCallback: error => {
+            console.error(error)
+            this.loading = false
+          }
         }
-      }
-    })
+      })
+    }
   }
 }
 </script>
