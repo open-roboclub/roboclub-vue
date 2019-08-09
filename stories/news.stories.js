d288ab48d679051592384752409fb00d68ae1682
iamareebjamal <jamal.areeb@gmail.com>
2019-08-10 00:10:36 +0530

    lint

--- stories/news.stories.js
+++ stories/news.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/vue'
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs'
 
 import NewsCard from '../components/news/Card.vue'
 
@@ -7,39 +7,37 @@ const stories = storiesOf('News Card', module)
 
 stories.addDecorator(withKnobs)
 
-stories
-  .add('default', () => {
-
-    return {
-      components: { NewsCard },
-      props: {
-        title: {
-          default: text('Title', 'This is a news')
-        },
-        date: {
-          default: text('Date', '29th August')
-        },
-        notice: {
-          default: text('Notice', 'In other news, we have gone with the wind')
-        },
-        link: {
-          default: text('Link', 'https://google.com')
-        }
+stories.add('default', () => {
+  return {
+    components: { NewsCard },
+    props: {
+      title: {
+        default: text('Title', 'This is a news')
       },
-      computed: {
-        news() {
-          return {
-            title: this.title,
-            date: this.date,
-            notice: this.notice,
-            link: this.link
-          }
-        }
+      date: {
+        default: text('Date', '29th August')
       },
-      template: `
+      notice: {
+        default: text('Notice', 'In other news, we have gone with the wind')
+      },
+      link: {
+        default: text('Link', 'https://google.com')
+      }
+    },
+    computed: {
+      news() {
+        return {
+          title: this.title,
+          date: this.date,
+          notice: this.notice,
+          link: this.link
+        }
+      }
+    },
+    template: `
       <v-flex xs4>
         <NewsCard :newsUpdate='news' />
       </v-flex>
       `
-    }
-  })
+  }
+})
