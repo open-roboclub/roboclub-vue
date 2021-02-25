48557465d8f0ac912c1bb5dd585131f01a728183
Areeb Jamal <jamal.areeb@gmail.com>
2021-02-26 04:53:04 +0530

    chore: Optimize build time

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    'nuxt-build-optimisations',
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
