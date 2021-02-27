a0a564cdb09bae765839dfbda1789f028c32acb3
Hashir Hameed Niazi <hameedhashirniazi@gmail.com>
2021-02-27 19:32:31 +0530

    refactor: Removed QR Code and Payment UPI

--- pages/apply.vue
+++ pages/apply.vue
@@ -79,71 +79,6 @@
               ></v-text-field>
             </v-col>
             <v-btn depressed color="primary" @click="apply"> Submit </v-btn>
-            <v-dialog v-model="dialog" max-width="650">
-              <v-card>
-                <v-card-title> Pay </v-card-title>
-                <v-row justify="center">
-                  <v-img
-                    src="https://firebasestorage.googleapis.com/v0/b/amu-roboclub.appspot.com/o/QR.png?alt=media&token=621bf3f7-624c-48be-bcf1-23bfb9c89125"
-                    cols="12"
-                    max-width="300"
-                  />
-                </v-row>
-                <v-row justify="center">
-                  <v-card color="#ffffba" max-width="75%">
-                    <v-container>
-                      <v-list-item>
-                        <div>
-                          <div class="font-weight-medium">
-                            Problem scanning QR?
-                          </div>
-                          Pay to
-                          <span class="font-weight-medium">
-                            UPI ID:
-                            <span class="font-italic"
-                              >prafullvarshney2000@oksbi</span
-                            >
-                          </span>
-                        </div>
-                      </v-list-item>
-
-                      <ul>
-                        <li>
-                          <div>Registration Fees:- ₹200</div>
-                        </li>
-                        <li>
-                          <div>Registration Validity:- 2 years</div>
-                        </li>
-                        <li>
-                          <div>
-                            During your payment, clearly mention your name and
-                            faculty number in note attached with it.
-                          </div>
-                        </li>
-                        <li>
-                          <div>
-                            If you face any difficulty with regards to payment,
-                            contact (+91) 99974 23637.
-                          </div>
-                        </li>
-                        <li>
-                          <div>
-                            Once your transaction is succesfull, we will reach
-                            you shortly.
-                          </div>
-                        </li>
-                      </ul>
-                    </v-container>
-                  </v-card>
-                </v-row>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="green darken-1" text @click="dialog = false">
-                    Done
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
           </v-row>
         </v-container>
       </v-form>
@@ -163,7 +98,6 @@ export default {
 
   data: () => ({
     valid: false,
-    dialog: false,
     rules,
     success: false,
     error: ''
@@ -197,7 +131,6 @@ export default {
         await this.addMember(false)
         this.$refs.form.reset()
         this.success = true
-        this.dialog = true
       } else if (this.error === '') {
         this.error = 'Please fill up the form correctly before submitting.'
       }
